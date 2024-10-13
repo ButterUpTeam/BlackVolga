35fec4ed9318647a5d47e7b060250b43705443b1
Jakub Latusek <jk.latusek@gmail.com>
2024-10-13 17:10:53 +0200

    Remove commented out code

--- scripts/police.gd
+++ scripts/police.gd
@@ -8,7 +8,7 @@ func _physics_process(delta: float) -> void:
 	var destination = navigation.get_next_path_position()
 	var move_vector: Vector2 = destination - global_position
 	var obj_vector = Vector2.UP.rotated(rotation)
-	var angle_to = obj_vector.angle_to(move_vector) #.angle() - rotation + (PI / 2)
+	var angle_to = obj_vector.angle_to(move_vector)
 	if not abs(angle_to) < 0.1:
 		calculate_rotation(delta, -1 if angle_to < 0 else 1)
 	calculate_velocity(delta, true)
