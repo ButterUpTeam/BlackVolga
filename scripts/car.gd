d628f5b2745bc3092f4e9234bcaa7ed43b82424f
Jakub Latusek <jk.latusek@gmail.com>
2024-10-13 11:19:21 +0200

    Make car mooove

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,26 +1,40 @@
 extends CharacterBody2D
 
+@export var MAX_SPEED = 200.0
+@export var ACCELEARTION = 1000
+@export var ROTATION_SPEED = 240
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+class InputValues:
+	var right: bool
+	var left: bool
+	var direction: int
+	var up: bool
 
 func _physics_process(delta: float) -> void:
-	return
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var input = get_input()
+	calculate_rotation(delta, input)
+	calculate_velocity(delta, input)
 
 	move_and_slide()
+
+func calculate_velocity(delta: float, input: InputValues) -> void:
+	if input.up:
+		velocity += (Vector2.UP * ACCELEARTION * delta).rotated(rotation)
+		velocity = velocity if velocity.length() < MAX_SPEED else velocity.normalized() * MAX_SPEED
+	else:
+		velocity -= (Vector2.UP * ACCELEARTION * delta).rotated(rotation)
+		velocity = velocity if velocity.length() < 0 else velocity.normalized() * 0
+
+
+func calculate_rotation(delta: float, input: InputValues) -> void:
+	rotation_degrees += delta * ROTATION_SPEED * input.direction
+
+
+func get_input() -> InputValues:
+	var input = InputValues.new()
+	input.right = Input.is_action_pressed("right")
+	input.left = Input.is_action_pressed("left")
+	input.up = Input.is_action_pressed("up")
+	input.direction = int(input.right) - int(input.left)
+	return input
+	
